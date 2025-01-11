8b5c87c0cc240bf830d7df9ba270e4b9ea150bab
Sothyra Chan <schan256@my.centennialcollege.ca>
2025-01-10 23:25:59 -0500

    added click event listeners and created html tags for JS to access them

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // script.js
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
+
+const checkUserInput = () => {
+  if (userInput.value === "") {
+    alert("Please provide a phone number");
+  }
+};
+
+const clearUserInput = () => {
+    userInput.value = "";
+    result.innerHTML = "";
+};
+
+checkBtn.addEventListener("click", checkUserInput);
+clearBtn.addEventListener("click", clearUserInput);
